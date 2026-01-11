6640edc262f647b3c1b76c9efc8c02a253c5379e
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:37:30 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "container_app_environment_storages" {
+  description = "All container_app_environment_storage resources"
+  value       = azurerm_container_app_environment_storage.container_app_environment_storages
+}
+
