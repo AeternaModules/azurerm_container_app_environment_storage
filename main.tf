a782ca9dbe365a812092016c1d5fe3b33c9a28b2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:58:01 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+data "azurerm_key_vault_secret" "access_key" {
+  for_each     = { for k, v in var.container_app_environment_storages : k => v if v.access_key_key_vault_id != null && v.access_key_key_vault_secret_name != null }
+  name         = each.value.access_key_key_vault_secret_name
+  key_vault_id = each.value.access_key_key_vault_id
+}
 resource "azurerm_container_app_environment_storage" "container_app_environment_storages" {
   for_each = var.container_app_environment_storages
 
@@ -5,7 +10,7 @@ resource "azurerm_container_app_environment_storage" "container_app_environment_
   container_app_environment_id = each.value.container_app_environment_id
   name                         = each.value.name
   share_name                   = each.value.share_name
-  access_key                   = each.value.access_key
+  access_key                   = each.value.access_key != null ? each.value.access_key : try(data.azurerm_key_vault_secret.access_key[each.key].value, null)
   account_name                 = each.value.account_name
   nfs_server_url               = each.value.nfs_server_url
 }
--- variables.tf
+++ variables.tf
@@ -8,18 +8,22 @@ Required:
     - share_name
 Optional:
     - access_key
+    - access_key_key_vault_id (alternative to access_key - read from Key Vault instead)
+    - access_key_key_vault_secret_name (alternative to access_key - read from Key Vault instead)
     - account_name
     - nfs_server_url
 EOT
 
   type = map(object({
-    access_mode                  = string
-    container_app_environment_id = string
-    name                         = string
-    share_name                   = string
-    access_key                   = optional(string)
-    account_name                 = optional(string)
-    nfs_server_url               = optional(string)
+    access_mode                      = string
+    container_app_environment_id     = string
+    name                             = string
+    share_name                       = string
+    access_key                       = optional(string)
+    access_key_key_vault_id          = optional(string)
+    access_key_key_vault_secret_name = optional(string)
+    account_name                     = optional(string)
+    nfs_server_url                   = optional(string)
   }))
 }
 
