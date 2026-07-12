97f002e57ee041bfb6d672155dcc832c818beba5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,30 @@
-output "container_app_environment_storages" {
-  description = "All container_app_environment_storage resources"
-  value       = azurerm_container_app_environment_storage.container_app_environment_storages
-  sensitive   = true
-}
 output "container_app_environment_storages_access_key" {
-  description = "List of access_key values across all container_app_environment_storages"
-  value       = [for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : v.access_key]
+  description = "Map of access_key values across all container_app_environment_storages, keyed the same as var.container_app_environment_storages"
+  value       = { for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : k => v.access_key }
   sensitive   = true
 }
 output "container_app_environment_storages_access_mode" {
-  description = "List of access_mode values across all container_app_environment_storages"
-  value       = [for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : v.access_mode]
+  description = "Map of access_mode values across all container_app_environment_storages, keyed the same as var.container_app_environment_storages"
+  value       = { for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : k => v.access_mode }
 }
 output "container_app_environment_storages_account_name" {
-  description = "List of account_name values across all container_app_environment_storages"
-  value       = [for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : v.account_name]
+  description = "Map of account_name values across all container_app_environment_storages, keyed the same as var.container_app_environment_storages"
+  value       = { for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : k => v.account_name }
 }
 output "container_app_environment_storages_container_app_environment_id" {
-  description = "List of container_app_environment_id values across all container_app_environment_storages"
-  value       = [for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : v.container_app_environment_id]
+  description = "Map of container_app_environment_id values across all container_app_environment_storages, keyed the same as var.container_app_environment_storages"
+  value       = { for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : k => v.container_app_environment_id }
 }
 output "container_app_environment_storages_name" {
-  description = "List of name values across all container_app_environment_storages"
-  value       = [for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : v.name]
+  description = "Map of name values across all container_app_environment_storages, keyed the same as var.container_app_environment_storages"
+  value       = { for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : k => v.name }
 }
 output "container_app_environment_storages_nfs_server_url" {
-  description = "List of nfs_server_url values across all container_app_environment_storages"
-  value       = [for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : v.nfs_server_url]
+  description = "Map of nfs_server_url values across all container_app_environment_storages, keyed the same as var.container_app_environment_storages"
+  value       = { for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : k => v.nfs_server_url }
 }
 output "container_app_environment_storages_share_name" {
-  description = "List of share_name values across all container_app_environment_storages"
-  value       = [for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : v.share_name]
+  description = "Map of share_name values across all container_app_environment_storages, keyed the same as var.container_app_environment_storages"
+  value       = { for k, v in azurerm_container_app_environment_storage.container_app_environment_storages : k => v.share_name }
 }
 
